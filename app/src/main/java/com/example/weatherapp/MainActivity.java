deebef9035463089ee01fb23422020508fcf0c73
paxarpp <pavelpp1974@gmail.com>
2023-02-18 22:23:34 +0300

    fix warning

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,106 +2,87 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout green;
+    private LinearLayout yellow;
+    private LinearLayout red;
+    private Button button;
+    private Button button_reset;
+    private boolean running = false;
+    private int step = 1;
 
-    private EditText user_field;
-    private Button main_btn;
-    private TextView result_info;
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        user_field = findViewById(R.id.user_field);
-        main_btn = findViewById(R.id.main_btn);
-        result_info = findViewById(R.id.result_info);
+        green = findViewById(R.id.green);
+        yellow = findViewById(R.id.yellow);
+        red = findViewById(R.id.red);
+        button = findViewById(R.id.button);
+        button_reset = findViewById(R.id.button_reset);
+        button_reset.setVisibility(View.VISIBLE);
 
-        main_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (user_field.getText().toString().trim().equals("")) {
-                    Toast.makeText(MainActivity.this, R.string.no_user_input, Toast.LENGTH_LONG).show();
-                } else {
-                    String city = user_field.getText().toString();
-                    String key = "82b797b6ebc625032318e16f1b42c016";
-                    String url = "https://api.openweathermap.org/data/2.5/weather?g=" + city + "&appid=" + key + "&lang=ru";
-
-                    new GetUrlData().execute(url);
-                }
-            }
-        });
     }
 
-    private class GetUrlData extends AsyncTask<String, String, String> {
+    public void onClickStart(View view) {
+        if (!running) {
+            running = true;
+            button.setText(R.string.stop);
+            button_reset.setVisibility(View.INVISIBLE);
+            new Thread(() -> {
+                while (running) {
+                    try {
+                        if (step == 1) {
+                            green.setBackgroundColor(getResources().getColor(R.color.green));
+                            yellow.setBackgroundColor(getResources().getColor(R.color.gray));
+                            red.setBackgroundColor(getResources().getColor(R.color.gray));
+                        } else if (step == 2) {
+                            green.setBackgroundColor(getResources().getColor(R.color.gray));
+                            yellow.setBackgroundColor(getResources().getColor(R.color.yellow));
+                            red.setBackgroundColor(getResources().getColor(R.color.gray));
+                        } else {
+                            green.setBackgroundColor(getResources().getColor(R.color.gray));
+                            yellow.setBackgroundColor(getResources().getColor(R.color.gray));
+                            red.setBackgroundColor(getResources().getColor(R.color.red));
+                        }
+                        if (step < 3) {
+                            step++;
+                        } else {
+                            step = 1;
+                        }
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
 
-        protected void onPreExecute() {
-            super.onPreExecute();
-            result_info.setText(R.string.wait);
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
-
-            try {
-                URL url = new URL(strings[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
-
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line).append("\n");
                 }
-
-                return buffer.toString();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                 if (reader != null) {
-                     reader.close();
-                 }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
+            }).start();
+        } else {
+            running = false;
+            button.setText(R.string.start);
+            button_reset.setVisibility(View.VISIBLE);
         }
+    }
 
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            result_info.setText(result);
+    public void onClickReset(View view) {
+        if (!running) {
+            step = 1;
+            green.setBackgroundColor(getResources().getColor(R.color.gray));
+            yellow.setBackgroundColor(getResources().getColor(R.color.gray));
+            red.setBackgroundColor(getResources().getColor(R.color.gray));
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        running = false;
     }
 }
